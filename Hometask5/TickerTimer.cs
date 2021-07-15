32fa490ec73ad0ab7d8dcc7b39473f4f395697b6
Barbarisko <helen.fedchuk@gmail.com>
2021-07-15 17:38:31 +0300

    created TickerTimer basics

--- Hometask5/TickerTimer.cs
+++ Hometask5/TickerTimer.cs
@@ -1,17 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Hometask5
 {
     //Створити клас TickerTimer який містить
     // - методи StartTimer, StopTimer,
     // - подію TickEvent, що генерується через певний проміжок часу який задається властивістю Interval у вигляді мілісекунд.
-    //Продемонструвати роботу класу TickerTimer з виводом на консоль.
-    //Запуск і зупинка таймера задається через консоль
-    class Program
+    public class TickerTimer
     {
-        static void Main(string[] args)
+        private int interval;
+
+        public int Interval { get => interval; set => interval = value; }
+
+        public void StartTimer()
         {
-            Console.WriteLine("Hello World!");
+
         }
+        public void StopTimer()
+        {
+
+        }
+
     }
 }
